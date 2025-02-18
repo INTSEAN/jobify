4ffa4e78115e4500d5e26e14c45dc8723737f0c3
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2025-02-18 13:14:59 -0500

    docs: updated error page

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
 
 const ErrorPage = () => {
-  <div>
-    <h3>You have not provided your details kindly head back</h3>
-    <Link to="/">Homepage</Link>
-  </div>;
+  return (
+    <div>
+      <h3>You have not provided your details kindly head back</h3>
+      <Link to="/">Homepage</Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
